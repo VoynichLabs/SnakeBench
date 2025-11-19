7bf8943acfcf82349de74117b2ae6497a0770597
Greg Kamradt <gkamradt@gmail.com>
2025-11-19 07:40:40 -0800

    another cors

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,7 @@ else:
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://snakebench.com",
+        "frontend-production-caa0.up.railway.app"
     ]
 
 CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
