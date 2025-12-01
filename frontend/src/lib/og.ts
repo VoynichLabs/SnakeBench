362b8031cab593518ae3c474ab90a34dce8d671b
Greg Kamradt <gkamradt@gmail.com>
2025-12-01 08:22:36 -0800

    fixing build

--- frontend/src/lib/og.ts
+++ frontend/src/lib/og.ts
@@ -23,7 +23,7 @@ type OgFont = {
   name: string;
   data: ArrayBuffer;
   style: 'normal' | 'italic';
-  weight: number;
+  weight: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 };
 
 export async function getOgFonts(): Promise<OgFont[]> {
